de60b6a6d60d5a0ce31abb5ce34fba0c70a631ad
toygildinpv <protagonyst@mail.ru>
2021-07-04 17:49:57 +0400

    MainActivity конвертирована на Kotlin, добавлен фрагмент

---
+++ app/src/main/java/project/paveltoy/kotlin_lesson1/MainActivity.kt
@@ -0,0 +1,12 @@
+package project.paveltoy.kotlin_lesson1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import project.paveltoy.kotlin_lesson1.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/project/paveltoy/kotlin_lesson1/MainFragment.kt
@@ -0,0 +1,4 @@
+package project.paveltoy.kotlin_lesson1
+
+class MainFragment {
+}
