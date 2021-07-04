527751d1ad2f90e8f41f335cce6cdfd3e57b7e65
anarchyon <protagonyst@mail.ru>
2021-07-04 21:54:31 +0300

    Изучение Kotlin1. Добавил фрагмент с плохоньким калькулятором

--- app/src/main/java/project/paveltoy/kotlin_lesson1/MainActivity.kt
+++ app/src/main/java/project/paveltoy/kotlin_lesson1/MainActivity.kt
@@ -2,11 +2,21 @@ package project.paveltoy.kotlin_lesson1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import project.paveltoy.kotlin_lesson1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button_execute).setOnClickListener{showFragment()}
+    }
+
+    private fun showFragment() {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container_main, MainFragment())
+                .commit()
     }
 }
